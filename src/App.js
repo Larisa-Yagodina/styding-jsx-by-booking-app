7a001785dcab6cae00cc16e5e484c9e3367b390d
larisayagodina <cheeseroll@mail.ru>
2024-02-16 10:37:57 +0300

    added variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,92 @@
 import './App.css';
 
+const ourAddress = {
+    zip: '90210',
+    email: 'booking@booking.com',
+    address: '124 West end blvd Los Angeles',
+    phoneNumber: '+19995553311',
+};
+const appMenu = ['Home', 'Air tickets', 'Accommodation', 'Leisure options', 'Car rental'];
+
+const footerMenu = ['About us', 'Contact us', 'Policy', 'Terms'];
+
+const accomodationTypes = ['Hotel', 'Bungalow', 'Bed and breakfast', 'Apartments'];
+
+const accomodation = [
+    {
+        id: '1',
+        name: 'Hilton',
+        description: 'beach front',
+        price: 150,
+        city: 'Los Angeles',
+        type: 'Hotel',
+    },
+    {
+        id: '2',
+        name: 'Radisson',
+        description: 'Resort and SPA',
+        price: 200,
+        city: 'New York',
+        type: 'Hotel',
+    },
+    {
+        id: '3',
+        name: 'Sheraton',
+        description: 'Best city view',
+        price: 220,
+        city: 'San Francisco',
+        type: 'Hotel',
+    },
+    {
+        id: '4',
+        name: 'Dream house',
+        description: 'Sea view',
+        price: 250,
+        city: 'Miami',
+        type: 'Bungalow',
+    },
+    {
+        id: '5',
+        name: 'Eden hills residence',
+        description: 'Garden view',
+        price: 320,
+        city: 'Clearwater beach',
+        type: 'Bungalow',
+    },
+    {
+        id: '6',
+        name: 'Motel 6',
+        description: 'cosy and convenient',
+        price: 60,
+        city: 'Thousand oaks',
+        type: 'Bed and breakfast',
+    },
+    {
+        id: '7',
+        name: 'Travelogy',
+        description: 'nice and romantic',
+        price: 90,
+        city: 'Sedona',
+        type: 'Bed and breakfast',
+    },
+    {
+        id: '8',
+        name: 'Gorgeous renovated apartment',
+        description: 'Breakfast included',
+        price: 590,
+        city: 'Chicago',
+        type: 'Apartments',
+    },
+    {
+        id: '9',
+        name: '4 bedroom apartment',
+        description: 'Home away from home',
+        price: 500,
+        city: 'New York',
+        type: 'Apartments',
+    },
+]
+
 // grid - сетка.
 // Грид представляет собой пересекающийся набор горизонтальных и вертикальных линий, образующих колонки и строки.
 // здесь задаем стиль для каталога, футера и меню (чтобы не один под другим, а колонками)
